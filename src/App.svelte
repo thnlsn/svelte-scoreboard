cd0088676a51495438399b34355236e134f674e8
Thomas Nelson <thnlsn@gmail.com>
2020-02-13 16:30:21 -0800

    added styling, removed boilerplate, added functiona; counter buttons[D

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,14 @@
 <script>
-  export let name; //export because it is passed in by another file
+  let name = "John Doe"; //export IF it is passed in by another file
+  let points = 100;
+
+  const addPoint = () => (points += 1); //being passed into the button on:click
+  const removePoint = () => (points -= 1); //being passed into the button on:click
 </script>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
   h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+    color: #204f6e;
   }
 
   @media (min-width: 640px) {
@@ -25,10 +19,15 @@
 </style>
 
 <main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
+
+  <div class="container">
+    <div class="card">
+      <h1>{name}</h1>
+      <h3>{points}</h3>
+      <button class="btn" on:click={addPoint}>+1</button>
+      <button class="btn btn-dark" on:click={removePoint}>+1</button>
+      <input type="number" bind:value={points} />
+    </div>
+  </div>
+
 </main>
