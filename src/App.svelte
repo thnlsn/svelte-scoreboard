60b5ebcc73d231e197fa5fb3d4684546e3b1f819
Thomas Nelson <thnlsn@gmail.com>
2020-02-13 17:31:46 -0800

    added if else to switch toggle controls buttons

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@
   let points = 100;
   let showControls = true;
 
+  const consoleLog = value => console.log(value);
+
   const addPoint = () => (points += 1); //being passed into the button on:click
   const removePoint = () => (points -= 1); //being passed into the button on:click
   const toggleControls = () => (showControls = !showControls); //set showControls boolean to whatever it is NOT at the time, so basically just flips it from true/false and vice versa
@@ -26,12 +28,14 @@
     <div class="card">
       <h1>
         {name}
-        <button class="btn btn-sm" on:click={toggleControls}>+</button>
+        <button class="btn btn-sm" on:click={toggleControls}>
+          {#if showControls}-{:else}+{/if}
+        </button>
       </h1>
       <h3>{points}</h3>
       {#if showControls}
         <button class="btn" on:click={addPoint}>+1</button>
-        <button class="btn btn-dark" on:click={removePoint}>+1</button>
+        <button class="btn btn-dark" on:click={removePoint}>-1</button>
         <input type="number" bind:value={points} />
       {/if}
     </div>
